d3d07dbb1419e798b4f71a06bdc4eaac86c8770a
walterthesmart <nwaugowalter@gmail.com>
2026-09-09 14:59:26 +0100

    chore: remove obsolete and unused files

--- scratch/alter.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {
-  await prisma.$executeRawUnsafe("ALTER TABLE SecurityTransaction ADD COLUMN symbol TEXT;");
-  console.log("Column added");
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
--- scratch/list-products.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const p = new PrismaClient();
-const products = await p.product.findMany();
-products.forEach(pr => console.log(pr.ticker, "|", pr.name, "|", pr.asset_class));
-await p.$disconnect();
